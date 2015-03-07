b93948b1aa91e209cc4ff5242e55f6e446ec6c17
sarah <sarahabimay@gmail.com>
2015-03-07 23:21:14 +0000

    components in own files

--- src/main.js
+++ src/main.js
@@ -6,17 +6,10 @@ var NotePadApp = require( './components/NotePadApp.js');
 var DummyComponent = require( './components/DummyComponent.js');
 
 
-
-
-
 // Render the SearchExample component on the page
-	React.render(
-		<NotePadApp url='library.json' pollInterval={2000} />, 
-		document.body
-	);
+React.render(
+	<NotePadApp url='library.json' pollInterval={2000} />, 
+	document.body
+);
 
-// React.render(
-// 	 <DummyComponent/>, document.body
-// 
-	// );
 
